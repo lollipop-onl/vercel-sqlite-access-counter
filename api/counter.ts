f56b505e9ad1651709fee0aa65c94d95d761ecdf
tamuraryoya <tamuraryoya@team-lab.com>
2020-07-23 14:37:37 +0900

    ⚡️ SQLiteの追加

--- api/counter.ts
+++ api/counter.ts
@@ -1,7 +1,33 @@
 import { NowRequest, NowResponse } from '@vercel/node';
+import { verbose } from 'sqlite3';
+
+const sqlite3 = verbose();
+const db = new sqlite3.Database(':memory:');
 
 export default function(req: NowRequest, res: NowResponse) {
-  const { name = 'World' } = req.query;
+  db.serialize(() => {
+    db.run('CREATE TABLE IF NOT EXISTS counter (count INT)');
 
-  res.send(`Hello ${name}!`);
+    let count = 0;
+
+    db.each('SELECT count FROM counter', (err, row) => {
+      if (err) {
+        console.error(err);
+
+        return;
+      }
+
+      count = row.count + 1;
+    });
+
+    const stmt = db.prepare('UPDATE counter SET count = ?');
+
+    stmt.run(count);
+
+    stmt.finalize();
+
+    const { name = 'World' } = req.query;
+
+    res.send(`Hello ${name}! (count ${count})`);
+  });
 }
